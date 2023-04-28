d396139dfe2511ed37750632caf70a63963671bb
Rotem Tamir <rotemtamir@gmail.com>
2023-04-28 08:16:31 +0300

    controllers: export the schema status (#4)

--- controllers/atlasschema_controller.go
+++ controllers/atlasschema_controller.go
@@ -19,7 +19,10 @@ package controllers
 import (
 	"bytes"
 	"context"
+	"crypto/sha256"
 	_ "embed"
+	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
@@ -33,16 +36,20 @@ import (
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 const (
-	devDBSuffix = "-atlas-dev-db"
-	hostReplace = "REPLACE_HOST"
+	devDBSuffix     = "-atlas-dev-db"
+	hostReplace     = "REPLACE_HOST"
+	schemaReadyCond = "SchemaReady"
 )
 
 var (
@@ -55,7 +62,7 @@ type (
 	// AtlasSchemaReconciler reconciles a AtlasSchema object
 	AtlasSchemaReconciler struct {
 		client.Client
-		CLI    *atlas.Client
+		CLI    Applier
 		Scheme *runtime.Scheme
 	}
 	// devDB contains values used to render a devDB pod template.
@@ -67,6 +74,14 @@ type (
 		DB          string
 		Port        int
 		UID         int
+	}
+	// desired contains information about the desired database schema.
+	desired struct {
+		ext    string
+		schema string
+	}
+	Applier interface {
+		SchemaApply(context.Context, *atlas.SchemaApplyParams) (*atlas.SchemaApply, error)
 	}
 )
 
@@ -83,17 +98,47 @@ type (
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *AtlasSchemaReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	sc := &dbv1alpha1.AtlasSchema{}
+	log := log.FromContext(ctx)
+	var (
+		sc  = &dbv1alpha1.AtlasSchema{}
+		des *desired
+		err error
+	)
 	if err := r.Get(ctx, req.NamespacedName, sc); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+	defer func() {
+		if err := r.Status().Update(ctx, sc); err != nil {
+			log.Error(err, "failed to update status")
+		}
+	}()
+	// When the resource is first created, create the "Ready" condition.
+	if sc.Status.Conditions == nil || len(sc.Status.Conditions) == 0 {
+		setNotReady(sc, "Reconciling", "Reconciling")
+		return ctrl.Result{Requeue: true}, nil
+	}
+	des, err = extractDesired(sc)
+	if err != nil {
+		setNotReady(sc, "ReadSchema", err.Error())
+		return ctrl.Result{}, err
+	}
+	// If the schema has changed and the schema's ready condition is not false, immediately set it to false.
+	// This is done so that the observed status of the schema reflects its "in-progress" state while it is being
+	// reconciled.
+	if !meta.IsStatusConditionFalse(sc.Status.Conditions, schemaReadyCond) && des.hash() != sc.Status.ObservedHash {
+		setNotReady(sc, "Reconciling", "current schema does not match last applied schema")
+		return ctrl.Result{Requeue: true}, nil
+	}
 	u, err := r.url(ctx, sc)
 	if err != nil {
+		setNotReady(sc, "ReadingTargetURL", err.Error())
 		return ctrl.Result{}, err
 	}
 	drv := driver(u.Scheme)
 	if drv == "" {
-		return ctrl.Result{}, fmt.Errorf("driver not found for scheme %q", u.Scheme)
+		err := fmt.Errorf("driver not found for scheme %q", u.Scheme)
+		setNotReady(sc, "ReadingDriver", err.Error())
+		return ctrl.Result{}, err
 	}
 	// make sure we have a dev db running
 	devDB := &v1.Deployment{}
@@ -101,6 +146,7 @@ func (r *AtlasSchemaReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	if apierrors.IsNotFound(err) {
 		devDB, err = r.devDBDeployment(ctx, sc, drv)
 		if err != nil {
+			setNotReady(sc, "CreatingDevDB", err.Error())
 			return ctrl.Result{}, err
 		}
 		return ctrl.Result{
@@ -108,15 +154,20 @@ func (r *AtlasSchemaReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		}, nil
 	}
 	if err != nil {
+		setNotReady(sc, "GettingDevDB", err.Error())
 		return ctrl.Result{}, err
 	}
 	devURL, err := r.devURL(ctx, req.Name, drv)
 	if err != nil {
+		setNotReady(sc, "GettingDevDBURL", err.Error())
 		return ctrl.Result{}, err
 	}
-	if err := r.apply(ctx, u.String(), devURL, sc); err != nil {
+	app, err := r.apply(ctx, u.String(), devURL, des)
+	if err != nil {
+		setNotReady(sc, "ApplyingSchema", err.Error())
 		return ctrl.Result{RequeueAfter: time.Second * 5}, err
 	}
+	setReady(sc, des, app)
 	return ctrl.Result{}, nil
 }
 
@@ -218,32 +269,17 @@ func driver(scheme string) string {
 	}
 }
 
-func (r *AtlasSchemaReconciler) apply(ctx context.Context, url, devURL string, tgt *dbv1alpha1.AtlasSchema) error {
-	var desired, ext string
-	switch sch := tgt.Spec.Schema; {
-	case sch.HCL != "":
-		desired = sch.HCL
-		ext = "hcl"
-	case sch.SQL != "":
-		desired = sch.SQL
-		ext = "sql"
-	default:
-		return fmt.Errorf("no schema specified")
-	}
-	file, clean, err := atlas.TempFile(desired, ext)
+func (r *AtlasSchemaReconciler) apply(ctx context.Context, url, devURL string, des *desired) (*atlas.SchemaApply, error) {
+	file, clean, err := atlas.TempFile(des.schema, des.ext)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer clean()
-	_, err = r.CLI.SchemaApply(ctx, &atlas.SchemaApplyParams{
+	return r.CLI.SchemaApply(ctx, &atlas.SchemaApplyParams{
 		URL:    url,
 		To:     file,
 		DevURL: devURL,
 	})
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (d *devDB) ConnTmpl() string {
@@ -258,4 +294,60 @@ func (d *devDB) ConnTmpl() string {
 		u.RawQuery = q.Encode()
 	}
 	return u.String()
+}
+
+// extractDesired extracts the desired schema from the AtlasSchema.
+func extractDesired(sc *dbv1alpha1.AtlasSchema) (*desired, error) {
+	var d desired
+	switch sch := sc.Spec.Schema; {
+	case sch.HCL != "":
+		d.schema = sch.HCL
+		d.ext = "hcl"
+	case sch.SQL != "":
+		d.schema = sch.SQL
+		d.ext = "sql"
+	default:
+		return nil, fmt.Errorf("no schema specified")
+	}
+	return &d, nil
+}
+
+// hash returns the sha256 hash of the schema.
+func (d *desired) hash() string {
+	h := sha256.New()
+	h.Write([]byte(d.schema))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func setNotReady(sc *dbv1alpha1.AtlasSchema, reason, msg string) {
+	meta.SetStatusCondition(
+		&sc.Status.Conditions,
+		metav1.Condition{
+			Type:    schemaReadyCond,
+			Status:  metav1.ConditionFalse,
+			Reason:  reason,
+			Message: msg,
+		},
+	)
+}
+
+func setReady(sc *dbv1alpha1.AtlasSchema, des *desired, apply *atlas.SchemaApply) {
+	msg := "The schema has been applied successfully."
+	if j, err := json.Marshal(apply); err != nil {
+		msg = fmt.Sprintf("%s. Error marshalling apply response: %v", msg,
+			err)
+	} else {
+		msg = fmt.Sprintf("%s. The schema has been applied successfully. Apply response: %s", msg, j)
+	}
+	meta.SetStatusCondition(
+		&sc.Status.Conditions,
+		metav1.Condition{
+			Type:    schemaReadyCond,
+			Status:  metav1.ConditionTrue,
+			Reason:  "Applied",
+			Message: msg,
+		},
+	)
+	sc.Status.ObservedHash = des.hash()
+	sc.Status.LastApplied = time.Now().Unix()
 }
--- controllers/controller_test.go
+++ controllers/controller_test.go
@@ -3,83 +3,292 @@ package controllers
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"reflect"
 	"testing"
+	"time"
 
 	dbv1alpha1 "github.com/ariga/atlas-operator/api/v1alpha1"
+	"github.com/ariga/atlas-operator/internal/atlas"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func TestReconcile(t *testing.T) {
-	scheme := runtime.NewScheme()
-	dbv1alpha1.AddToScheme(scheme)
-	mock := &MockClient{}
-	r := AtlasSchemaReconciler{
-		Client: mock,
-		Scheme: scheme,
+func TestReconcile_NotFound(t *testing.T) {
+	tt := newTest(t)
+	resp, err := tt.r.Reconcile(context.Background(), req())
+	require.NoError(t, err)
+	require.EqualValues(t, ctrl.Result{}, resp)
+}
+
+func TestReconcile_NoCond(t *testing.T) {
+	tt := newTest(t)
+	tt.m.put(&dbv1alpha1.AtlasSchema{
+		ObjectMeta: objmeta(),
+	})
+	request := req()
+	resp, err := tt.r.Reconcile(context.Background(), request)
+	require.NoError(t, err)
+	require.EqualValues(t, ctrl.Result{Requeue: true}, resp)
+	cond := tt.cond()
+	require.EqualValues(t, schemaReadyCond, cond.Type)
+	require.EqualValues(t, metav1.ConditionFalse, cond.Status)
+	require.EqualValues(t, "Reconciling", cond.Message)
+}
+
+func TestReconcile_ReadyButDiff(t *testing.T) {
+	tt := newTest(t)
+	tt.m.put(&dbv1alpha1.AtlasSchema{
+		ObjectMeta: objmeta(),
+		Spec: dbv1alpha1.AtlasSchemaSpec{
+			Schema: dbv1alpha1.Schema{SQL: "create table foo (id int primary key);"},
+		},
+		Status: dbv1alpha1.AtlasSchemaStatus{
+			ObservedHash: "old",
+			Conditions: []metav1.Condition{
+				{
+					Type:   schemaReadyCond,
+					Status: metav1.ConditionTrue,
+				},
+			},
+		},
+	})
+	request := req()
+	resp, err := tt.r.Reconcile(context.Background(), request)
+	require.NoError(t, err)
+	require.EqualValues(t, ctrl.Result{Requeue: true}, resp)
+	cond := tt.cond()
+	require.EqualValues(t, schemaReadyCond, cond.Type)
+	require.EqualValues(t, metav1.ConditionFalse, cond.Status)
+}
+
+func TestReconcile_NoSchema(t *testing.T) {
+	tt := newTest(t)
+	tt.m.put(&dbv1alpha1.AtlasSchema{
+		ObjectMeta: objmeta(),
+		Status: dbv1alpha1.AtlasSchemaStatus{
+			Conditions: []metav1.Condition{
+				{
+					Type:   schemaReadyCond,
+					Status: metav1.ConditionFalse,
+				},
+			},
+		},
+	})
+	request := req()
+	_, err := tt.r.Reconcile(context.Background(), request)
+	require.EqualError(t, err, "no schema specified")
+}
+
+func TestReconcile_HasSchema(t *testing.T) {
+	tt := newTest(t)
+	tt.m.put(reconcilingSchema())
+	request := req()
+	resp, err := tt.r.Reconcile(context.Background(), request)
+	require.NoError(t, err)
+	require.EqualValues(t, ctrl.Result{RequeueAfter: time.Second * 15}, resp)
+	d := tt.m.state[types.NamespacedName{
+		Namespace: request.Namespace,
+		Name:      request.Name + devDBSuffix,
+	}].(*appsv1.Deployment)
+	require.EqualValues(t, "mysql:8", d.Spec.Template.Spec.Containers[0].Image)
+}
+
+func TestReconcile_HasSchemaAndDB(t *testing.T) {
+	tt := newTest(t)
+	tt.m.put(reconcilingSchema())
+	tt.m.put(&appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-atlas-schema" + devDBSuffix,
+			Namespace: "test",
+		},
+		Status: appsv1.DeploymentStatus{
+			ReadyReplicas: 1,
+		},
+	})
+	request := req()
+	resp, err := tt.r.Reconcile(context.Background(), request)
+	require.NoError(t, err)
+	require.EqualValues(t, ctrl.Result{}, resp)
+	cond := tt.cond()
+	require.EqualValues(t, schemaReadyCond, cond.Type)
+	require.EqualValues(t, metav1.ConditionTrue, cond.Status)
+	require.EqualValues(t, "Applied", cond.Reason)
+}
+
+func reconcilingSchema() *dbv1alpha1.AtlasSchema {
+	return &dbv1alpha1.AtlasSchema{
+		ObjectMeta: objmeta(),
+		Spec: dbv1alpha1.AtlasSchemaSpec{
+			URL:    "mysql://root:password@localhost:3306/test",
+			Schema: dbv1alpha1.Schema{SQL: "CREATE TABLE foo (id INT PRIMARY KEY);"},
+		},
+		Status: dbv1alpha1.AtlasSchemaStatus{
+			Conditions: []metav1.Condition{
+				{
+					Type:   schemaReadyCond,
+					Status: metav1.ConditionFalse,
+				},
+			},
+		},
 	}
-	_, err := r.Reconcile(context.Background(), ctrl.Request{
+}
+func objmeta() metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name:      "my-atlas-schema",
+		Namespace: "test",
+	}
+}
+
+func req() ctrl.Request {
+	return ctrl.Request{
 		NamespacedName: client.ObjectKey{
 			Name:      "my-atlas-schema",
 			Namespace: "test",
 		},
-	})
-	require.NoError(t, err)
-	require.EqualValues(t, mock.created.Name, "my-atlas-schema"+devDBSuffix)
-	require.EqualValues(t, mock.created.Spec.Template.Spec.Containers[0].Image, "mysql:8")
+	}
 }
 
-type MockClient struct {
-	client.Client
-	created *appsv1.Deployment
+type test struct {
+	*testing.T
+	m *mockClient
+	r *AtlasSchemaReconciler
 }
 
-func (m *MockClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
-	switch obj.(type) {
-	case *dbv1alpha1.AtlasSchema:
-		as := &dbv1alpha1.AtlasSchema{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: key.Namespace,
-				Name:      key.Name,
-			},
-			Spec: dbv1alpha1.AtlasSchemaSpec{
-				URLFrom: dbv1alpha1.URLFrom{
-					SecretKeyRef: &v1.SecretKeySelector{
-						LocalObjectReference: v1.LocalObjectReference{Name: "test-secret"},
-						Key:                  "url",
-					},
-				},
-			},
-		}
-		*obj.(*dbv1alpha1.AtlasSchema) = *as
-	case *v1.Secret:
-		s := &v1.Secret{
-			Data: map[string][]byte{
-				"url": []byte("mysql://root:pass@/test"),
-			},
-		}
-		*obj.(*v1.Secret) = *s
-	default:
+func newTest(t *testing.T) *test {
+	scheme := runtime.NewScheme()
+	dbv1alpha1.AddToScheme(scheme)
+	m := &mockClient{
+		state: map[client.ObjectKey]client.Object{},
+	}
+	return &test{
+		T: t,
+		m: m,
+		r: &AtlasSchemaReconciler{
+			Client: m,
+			Scheme: scheme,
+			CLI:    &mockApplier{},
+		},
+	}
+}
+
+type (
+	mockClient struct {
+		client.Client
+		state map[client.ObjectKey]client.Object
+	}
+	mockSubResourceWriter struct {
+		client.SubResourceWriter
+		ref *mockClient
+	}
+	mockApplier struct {
+		Applier
+	}
+)
+
+func (m *mockClient) put(obj client.Object) {
+	m.state[client.ObjectKeyFromObject(obj)] = obj
+}
+
+func (m *mockClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
+	// retrieve the object from the state map
+	o, ok := m.state[key]
+	if !ok {
 		return errors.NewNotFound(schema.GroupResource{
 			Group: obj.GetObjectKind().GroupVersionKind().Group,
 		}, key.Name)
 	}
+	// if o and obj are the same type, just copy o into obj
+	if reflect.TypeOf(o) == reflect.TypeOf(obj) {
+		reflect.ValueOf(obj).Elem().Set(reflect.ValueOf(o).Elem())
+		return nil
+	}
 	return nil
 }
 
-func (m *MockClient) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
-	switch obj.(type) {
-	case *appsv1.Deployment:
-		m.created = obj.(*appsv1.Deployment)
+// Hardcoded list of pods to simulate a running dev db.
+func (m *mockClient) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
+	if reflect.TypeOf(list) != reflect.TypeOf(&corev1.PodList{}) {
+		return fmt.Errorf("unsupported list type: %T", list)
 	}
+	podList := list.(*corev1.PodList)
+	podList.Items = []corev1.Pod{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "my-atlas-schema" + devDBSuffix,
+				Namespace: "test",
+				Annotations: map[string]string{
+					"atlasgo.io/conntmpl": "mysql://root:password@" + hostReplace + ":3306/test",
+				},
+			},
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name: "mysql",
+						Ports: []corev1.ContainerPort{
+							{ContainerPort: 3306},
+						},
+					},
+				},
+			},
+			Status: corev1.PodStatus{
+				PodIP: "1.2.3.4",
+				Phase: corev1.PodRunning,
+			},
+		},
+	}
+	return nil
+}
+
+func (m *mockClient) Status() client.StatusWriter {
+	return &mockSubResourceWriter{
+		ref: m,
+	}
+}
+
+func (s *mockSubResourceWriter) Update(ctx context.Context, obj client.Object, opts ...client.SubResourceUpdateOption) error {
+	s.ref.put(obj)
+	return nil
+}
+
+func TestMock(t *testing.T) {
+	tt := newTest(t)
+	tt.m.put(&appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: pointer.Int32(1),
+		},
+	})
+	var d appsv1.Deployment
+	// Req a non existent object
+	err := tt.m.Get(context.Background(), client.ObjectKey{
+		Name: "non-existent",
+	}, &d)
+	require.True(t, errors.IsNotFound(err))
+	// Retrieve an existing object
+	err = tt.m.Get(context.Background(), client.ObjectKey{
+		Name:      "test",
+		Namespace: "default",
+	}, &d)
+	require.NoError(t, err)
+	require.EqualValues(t, d.Spec.Replicas, pointer.Int32(1))
+}
+
+func (m *mockClient) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+	m.put(obj)
 	return nil
 }
 
@@ -100,4 +309,13 @@ func TestTemplateSanity(t *testing.T) {
 			b.Reset()
 		})
 	}
+}
+
+func (a *mockApplier) SchemaApply(context.Context, *atlas.SchemaApplyParams) (*atlas.SchemaApply, error) {
+	return &atlas.SchemaApply{}, nil
+}
+
+func (t *test) cond() metav1.Condition {
+	s := t.m.state[req().NamespacedName].(*dbv1alpha1.AtlasSchema)
+	return s.Status.Conditions[0]
 }
