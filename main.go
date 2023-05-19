701fe90648c666944be365769fec9b280460d061
datdao <datdao.sde@gmail.com>
2023-05-19 16:05:19 +0700

    all: initialize atlas migration

--- main.go
+++ main.go
@@ -112,6 +112,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AtlasSchema")
 		os.Exit(1)
 	}
+	if err = (&controllers.AtlasMigrationReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AtlasMigration")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
