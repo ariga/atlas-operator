1c38a4350a7ae2b6f058c89013cf99eb47d21623
Giau. Tran Minh <hello@giautm.dev>
2023-08-04 20:23:34 +0700

    api/v1alpha1: add helpers and tests

--- api/v1alpha1/atlasschema_types.go
+++ api/v1alpha1/atlasschema_types.go
@@ -17,166 +17,123 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"net/url"
+	"path/filepath"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// AtlasSchemaSpec defines the desired state of AtlasSchema
-type AtlasSchemaSpec struct {
-	// URL of the target database schema.
-	URL string `json:"url,omitempty"`
-	// URLs may be defined as a secret key reference.
-	URLFrom URLFrom `json:"urlFrom,omitempty"`
-	// Credentials defines the credentials to use when connecting to the database.
-	// Used instead of URL or URLFrom.
-	Credentials Credentials `json:"credentials,omitempty"`
-	// Desired Schema of the target.
-	Schema Schema `json:"schema,omitempty"`
-	// Exclude a list of glob patterns used to filter existing resources being taken into account.
-	Exclude []string `json:"exclude,omitempty"`
+type (
+	//+kubebuilder:object:root=true
+	//
+	// AtlasSchemaList contains a list of AtlasSchema
+	AtlasSchemaList struct {
+		metav1.TypeMeta `json:",inline"`
+		metav1.ListMeta `json:"metadata,omitempty"`
+
+		Items []AtlasSchema `json:"items"`
+	}
+	//+kubebuilder:object:root=true
+	//+kubebuilder:subresource:status
+	//
+	// AtlasSchema is the Schema for the atlasschemas API
+	// +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=`.status.conditions[?(@.type=="Ready")].status`
+	// +kubebuilder:printcolumn:name="Reason",type=string,JSONPath=`.status.conditions[?(@.type=="Ready")].reason`
+	AtlasSchema struct {
+		metav1.TypeMeta   `json:",inline"`
+		metav1.ObjectMeta `json:"metadata,omitempty"`
+
+		Spec   AtlasSchemaSpec   `json:"spec,omitempty"`
+		Status AtlasSchemaStatus `json:"status,omitempty"`
+	}
+	// AtlasSchemaStatus defines the observed state of AtlasSchema
+	AtlasSchemaStatus struct {
+		// Conditions represent the latest available observations of an object's state.
+		Conditions []metav1.Condition `json:"conditions,omitempty"`
+		// ObservedHash is the hash of the most recently applied schema.
+		ObservedHash string `json:"observed_hash"`
+		// LastApplied is the unix timestamp of the most recent successful schema apply operation.
+		LastApplied int64 `json:"last_applied"`
+	}
+	// AtlasSchemaSpec defines the desired state of AtlasSchema
+	AtlasSchemaSpec struct {
+		TargetSpec `json:",inline"`
+		// Desired Schema of the target.
+		Schema Schema `json:"schema,omitempty"`
+		// Exclude a list of glob patterns used to filter existing resources being taken into account.
+		Exclude []string `json:"exclude,omitempty"`
+		// Policy defines the policies to apply when managing the schema change lifecycle.
+		Policy Policy `json:"policy,omitempty"`
+		// The names of the schemas (named databases) on the target database to be managed.
+		Schemas []string `json:"schemas,omitempty"`
+	}
+	// Schema defines the desired state of the target database schema in plain SQL or HCL.
+	Schema struct {
+		SQL             string                       `json:"sql,omitempty"`
+		HCL             string                       `json:"hcl,omitempty"`
+		ConfigMapKeyRef *corev1.ConfigMapKeySelector `json:"configMapKeyRef,omitempty"`
+	}
 	// Policy defines the policies to apply when managing the schema change lifecycle.
-	Policy Policy `json:"policy,omitempty"`
-	// The names of the schemas (named databases) on the target database to be managed.
-	Schemas []string `json:"schemas,omitempty"`
-}
-
-// Credentials defines the credentials to use when connecting to the database.
-type Credentials struct {
-	Scheme       string            `json:"scheme,omitempty"`
-	User         string            `json:"user,omitempty"`
-	Password     string            `json:"password,omitempty"`
-	PasswordFrom PasswordFrom      `json:"passwordFrom,omitempty"`
-	Host         string            `json:"host,omitempty"`
-	Port         int               `json:"port,omitempty"`
-	Database     string            `json:"database,omitempty"`
-	Parameters   map[string]string `json:"parameters,omitempty"`
-}
-
-// PasswordFrom references a key containing the password.
-type PasswordFrom struct {
-	// SecretKeyRef defines the secret key reference to use for the password.
-	SecretKeyRef *corev1.SecretKeySelector `json:"secretKeyRef,omitempty"`
-}
-
-// URL returns the URL for the database.
-func (c *Credentials) URL() *url.URL {
-	u := &url.URL{
-		Scheme: c.Scheme,
-		Path:   c.Database,
+	Policy struct {
+		Lint Lint `json:"lint,omitempty"`
+		Diff Diff `json:"diff,omitempty"`
 	}
-	if c.User != "" || c.Password != "" {
-		u.User = url.UserPassword(c.User, c.Password)
+	// Lint defines the linting policies to apply before applying the schema.
+	Lint struct {
+		Destructive CheckConfig `json:"destructive,omitempty"`
 	}
-	if len(c.Parameters) > 0 {
-		qs := url.Values{}
-		for k, v := range c.Parameters {
-			qs.Set(k, v)
-		}
-		u.RawQuery = qs.Encode()
+	// CheckConfig defines the configuration of a linting check.
+	CheckConfig struct {
+		Error bool `json:"error,omitempty"`
 	}
-	host := c.Host
-	if c.Port > 0 {
-		host = fmt.Sprintf("%s:%d", host, c.Port)
+	// Diff defines the diff policies to apply when planning schema changes.
+	Diff struct {
+		Skip SkipChanges `json:"skip,omitempty"`
 	}
-	u.Host = host
-	return u
-}
+	// SkipChanges represents the skip changes policy.
+	SkipChanges struct {
+		// +optional
+		AddSchema bool `json:"add_schema,omitempty"`
+		// +optional
+		DropSchema bool `json:"drop_schema,omitempty"`
+		// +optional
+		ModifySchema bool `json:"modify_schema,omitempty"`
+		// +optional
+		AddTable bool `json:"add_table,omitempty"`
+		// +optional
+		DropTable bool `json:"drop_table,omitempty"`
+		// +optional
+		ModifyTable bool `json:"modify_table,omitempty"`
+		// +optional
+		AddColumn bool `json:"add_column,omitempty"`
+		// +optional
+		DropColumn bool `json:"drop_column,omitempty"`
+		// +optional
+		ModifyColumn bool `json:"modify_column,omitempty"`
+		// +optional
+		AddIndex bool `json:"add_index,omitempty"`
+		// +optional
+		DropIndex bool `json:"drop_index,omitempty"`
+		// +optional
+		ModifyIndex bool `json:"modify_index,omitempty"`
+		// +optional
+		AddForeignKey bool `json:"add_foreign_key,omitempty"`
+		// +optional
+		DropForeignKey bool `json:"drop_foreign_key,omitempty"`
+		// +optional
+		ModifyForeignKey bool `json:"modify_foreign_key,omitempty"`
+	}
+)
 
-// Policy defines the policies to apply when managing the schema change lifecycle.
-type Policy struct {
-	Lint Lint `json:"lint,omitempty"`
-	Diff Diff `json:"diff,omitempty"`
-}
-
-// Lint defines the linting policies to apply before applying the schema.
-type Lint struct {
-	Destructive CheckConfig `json:"destructive,omitempty"`
-}
-
-// Diff defines the diff policies to apply when planning schema changes.
-type Diff struct {
-	Skip SkipChanges `json:"skip,omitempty"`
-}
-
-// SkipChanges represents the skip changes policy.
-type SkipChanges struct {
-	// +optional
-	AddSchema bool `json:"add_schema,omitempty"`
-	// +optional
-	DropSchema bool `json:"drop_schema,omitempty"`
-	// +optional
-	ModifySchema bool `json:"modify_schema,omitempty"`
-	// +optional
-	AddTable bool `json:"add_table,omitempty"`
-	// +optional
-	DropTable bool `json:"drop_table,omitempty"`
-	// +optional
-	ModifyTable bool `json:"modify_table,omitempty"`
-	// +optional
-	AddColumn bool `json:"add_column,omitempty"`
-	// +optional
-	DropColumn bool `json:"drop_column,omitempty"`
-	// +optional
-	ModifyColumn bool `json:"modify_column,omitempty"`
-	// +optional
-	AddIndex bool `json:"add_index,omitempty"`
-	// +optional
-	DropIndex bool `json:"drop_index,omitempty"`
-	// +optional
-	ModifyIndex bool `json:"modify_index,omitempty"`
-	// +optional
-	AddForeignKey bool `json:"add_foreign_key,omitempty"`
-	// +optional
-	DropForeignKey bool `json:"drop_foreign_key,omitempty"`
-	// +optional
-	ModifyForeignKey bool `json:"modify_foreign_key,omitempty"`
-}
-
-// CheckConfig defines the configuration of a linting check.
-type CheckConfig struct {
-	Error bool `json:"error,omitempty"`
-}
-
-// URLFrom defines a reference to a secret key that contains the Atlas URL of the
-// target database schema.
-type URLFrom struct {
-	// SecretKeyRef references to the key of a secret in the same namespace.
-	SecretKeyRef *corev1.SecretKeySelector `json:"secretKeyRef,omitempty"`
-}
-
-// Schema defines the desired state of the target database schema in plain SQL or HCL.
-type Schema struct {
-	SQL             string                       `json:"sql,omitempty"`
-	HCL             string                       `json:"hcl,omitempty"`
-	ConfigMapKeyRef *corev1.ConfigMapKeySelector `json:"configMapKeyRef,omitempty"`
-}
-
-// AtlasSchemaStatus defines the observed state of AtlasSchema
-type AtlasSchemaStatus struct {
-	// Conditions represent the latest available observations of an object's state.
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
-	// ObservedHash is the hash of the most recently applied schema.
-	ObservedHash string `json:"observed_hash"`
-	// LastApplied is the unix timestamp of the most recent successful schema apply operation.
-	LastApplied int64 `json:"last_applied"`
-}
-
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-
-// AtlasSchema is the Schema for the atlasschemas API
-// +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=`.status.conditions[?(@.type=="Ready")].status`
-// +kubebuilder:printcolumn:name="Reason",type=string,JSONPath=`.status.conditions[?(@.type=="Ready")].reason`
-type AtlasSchema struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   AtlasSchemaSpec   `json:"spec,omitempty"`
-	Status AtlasSchemaStatus `json:"status,omitempty"`
+func init() {
+	SchemeBuilder.Register(&AtlasSchema{}, &AtlasSchemaList{})
 }
 
 // NamespacedName returns the namespaced name of the object.
@@ -187,15 +144,67 @@ func (s *AtlasSchema) NamespacedName() types.NamespacedName {
 	}
 }
 
-//+kubebuilder:object:root=true
-
-// AtlasSchemaList contains a list of AtlasSchema
-type AtlasSchemaList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []AtlasSchema `json:"items"`
+// IsReady returns true if the ready condition is true.
+func (m *AtlasSchema) IsReady() bool {
+	return meta.IsStatusConditionTrue(m.Status.Conditions, readyCond)
 }
 
-func init() {
-	SchemeBuilder.Register(&AtlasSchema{}, &AtlasSchemaList{})
+// IsHashModified returns true if the hash is different from the observed hash.
+func (sc *AtlasSchema) IsHashModified(hash string) bool {
+	return hash != sc.Status.ObservedHash
+}
+
+// SetReady sets the Ready condition to true
+func (sc *AtlasSchema) SetReady(status AtlasSchemaStatus, report any) {
+	var msg string
+	if j, err := json.Marshal(report); err != nil {
+		msg = fmt.Sprintf("Error marshalling apply response: %v", err)
+	} else {
+		msg = fmt.Sprintf("The schema has been applied successfully. Apply response: %s", j)
+	}
+	sc.Status = status
+	meta.SetStatusCondition(&sc.Status.Conditions, metav1.Condition{
+		Type:    readyCond,
+		Status:  metav1.ConditionTrue,
+		Reason:  "Applied",
+		Message: msg,
+	})
+}
+
+// SetNotReady sets the Ready condition to false
+// with the given reason and message.
+func (sc *AtlasSchema) SetNotReady(reason, msg string) {
+	meta.SetStatusCondition(&sc.Status.Conditions, metav1.Condition{
+		Type:    readyCond,
+		Status:  metav1.ConditionFalse,
+		Reason:  reason,
+		Message: msg,
+	})
+}
+
+// Content returns the desired schema of the AtlasSchema.
+func (s Schema) Content(ctx context.Context, r client.Reader, ns string) ([]byte, string, error) {
+	switch ref := s.ConfigMapKeyRef; {
+	case ref != nil:
+		val := &corev1.ConfigMap{}
+		err := r.Get(ctx, types.NamespacedName{Name: ref.Name, Namespace: ns}, val)
+		if err != nil {
+			return nil, "", err
+		}
+		// Guess the schema file format based on the key's extension.
+		ext := strings.ToLower(filepath.Ext(ref.Key))
+		switch desired, ok := val.Data[ref.Key]; {
+		case !ok:
+			return nil, "", fmt.Errorf("configmaps %s/%s does not contain key %q", ns, ref.Name, ref.Key)
+		case ext == ".hcl" || ext == ".sql":
+			return []byte(desired), ext[1:], nil
+		default:
+			return nil, "", fmt.Errorf("configmaps key %q must be ending with .sql or .hcl, received %q", ref.Key, ext)
+		}
+	case s.HCL != "":
+		return []byte(s.HCL), "hcl", nil
+	case s.SQL != "":
+		return []byte(s.SQL), "sql", nil
+	}
+	return nil, "", fmt.Errorf("no desired schema specified")
 }
--- api/v1alpha1/types_test.go
+++ api/v1alpha1/types_test.go
@@ -1,18 +1,146 @@
-package v1alpha1
+package v1alpha1_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	testclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/ariga/atlas-operator/api/v1alpha1"
 )
+
+// This test ensure the priority of methods
+// to get the database URL:
+// URLFrom > URL > Credentials.PasswordFrom > Credentials > error
+func TestTargetSpec_DatabaseURL(t *testing.T) {
+	var (
+		ctx    = context.Background()
+		target = v1alpha1.TargetSpec{}
+		client = testclient.NewClientBuilder().
+			WithObjects(&v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"url":      []byte("mysql://root:root@localhost:3306/secret"),
+					"password": []byte("123456"),
+				},
+			}).
+			Build()
+		equal = func(a string) {
+			u, err := target.DatabaseURL(ctx, client, "default")
+			require.NoError(t, err)
+			require.Equal(t, a, u.String())
+		}
+	)
+	// error
+	_, err := target.DatabaseURL(ctx, nil, "default")
+	require.ErrorContains(t, err, "no target database defined")
+
+	// Should return the URL from the credentials
+	target.Credentials = v1alpha1.Credentials{
+		Scheme:   "mysql",
+		Host:     "localhost",
+		Port:     3306,
+		Database: "local",
+		User:     "nobody",
+		Password: "secret",
+	}
+	equal("mysql://nobody:secret@localhost:3306/local")
+
+	// Should return the URL from the credentials and the password from the secret
+	target.Credentials.PasswordFrom.SecretKeyRef = &v1.SecretKeySelector{
+		LocalObjectReference: v1.LocalObjectReference{
+			Name: "test",
+		},
+		Key: "password",
+	}
+	equal("mysql://nobody:123456@localhost:3306/local")
+
+	// Should return the same URL if explicitly defined
+	target.URL = "mysql://root:root@localhost:3306/test"
+	equal(target.URL)
+
+	// Should return the URL from the secret
+	target.URLFrom.SecretKeyRef = &v1.SecretKeySelector{
+		LocalObjectReference: v1.LocalObjectReference{
+			Name: "test",
+		},
+		Key: "url",
+	}
+	equal("mysql://root:root@localhost:3306/secret")
+}
+
+func TestSchema_Content(t *testing.T) {
+	var (
+		ctx    = context.Background()
+		sch    = v1alpha1.Schema{}
+		client = testclient.NewClientBuilder().
+			WithObjects(&v1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "default",
+				},
+				Data: map[string]string{
+					"schema.bug": `boo`,
+					"schema.hcl": `foo`,
+					"schema.sql": `bar`,
+				},
+			}).
+			Build()
+	)
+	// error
+	_, _, err := sch.Content(ctx, client, "default")
+	require.ErrorContains(t, err, "no desired schema specified")
+
+	sch.SQL = "bar"
+	data, ext, err := sch.Content(ctx, client, "default")
+	require.NoError(t, err)
+	require.Equal(t, "sql", ext)
+	require.Equal(t, []byte("bar"), data)
+
+	sch.HCL = "foo"
+	data, ext, err = sch.Content(ctx, client, "default")
+	require.NoError(t, err)
+	require.Equal(t, "hcl", ext)
+	require.Equal(t, []byte("foo"), data)
+
+	// Should return the content from the configmap
+	sch.ConfigMapKeyRef = &v1.ConfigMapKeySelector{
+		LocalObjectReference: v1.LocalObjectReference{
+			Name: "test",
+		},
+		Key: "schema.sql",
+	}
+	data, ext, err = sch.Content(ctx, client, "default")
+	require.NoError(t, err)
+	require.Equal(t, "sql", ext)
+	require.Equal(t, []byte("bar"), data)
+
+	sch.ConfigMapKeyRef.Key = "schema.bug"
+	_, _, err = sch.Content(ctx, client, "default")
+	require.ErrorContains(t, err, `configmaps key "schema.bug" must be ending with .sql or .hcl, received ".bug"`)
+
+	sch.ConfigMapKeyRef.Key = "schema.foo"
+	_, _, err = sch.Content(ctx, client, "default")
+	require.ErrorContains(t, err, `configmaps default/test does not contain key "schema.foo"`)
+
+	sch.ConfigMapKeyRef.Name = "foo"
+	_, _, err = sch.Content(ctx, client, "default")
+	require.ErrorContains(t, err, `configmaps "foo" not found`)
+}
 
 func TestCredentials_URL(t *testing.T) {
 	for _, tt := range []struct {
-		c   Credentials
+		c   v1alpha1.Credentials
 		exp string
 	}{
 		{
-			c: Credentials{
+			c: v1alpha1.Credentials{
 				Scheme:   "postgres",
 				User:     "user",
 				Password: "pass",
@@ -26,7 +154,7 @@ func TestCredentials_URL(t *testing.T) {
 			exp: "postgres://user:pass@host:5432/db?sslmode=disable",
 		},
 		{
-			c: Credentials{
+			c: v1alpha1.Credentials{
 				Scheme: "sqlite",
 				Host:   "file",
 				Parameters: map[string]string{
@@ -36,7 +164,7 @@ func TestCredentials_URL(t *testing.T) {
 			exp: "sqlite://file?mode=memory",
 		},
 		{
-			c: Credentials{
+			c: v1alpha1.Credentials{
 				Scheme:   "mysql",
 				User:     "user",
 				Password: "pass",
@@ -46,7 +174,7 @@ func TestCredentials_URL(t *testing.T) {
 			exp: "mysql://user:pass@host/db",
 		},
 		{
-			c: Credentials{
+			c: v1alpha1.Credentials{
 				Scheme:   "mysql",
 				User:     "user",
 				Password: "pass",
