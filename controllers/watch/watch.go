cd32534c1bba34d3dcfc30f4c37d1aa9d9bdf8e0
Giau. Tran Minh <12751435+giautm@users.noreply.github.com>
2024-04-11 18:44:49 +0700

    all: remove `exp/slices` (#168)

--- controllers/watch/watch.go
+++ controllers/watch/watch.go
@@ -15,7 +15,8 @@
 package watch
 
 import (
-	"golang.org/x/exp/slices"
+	"slices"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
