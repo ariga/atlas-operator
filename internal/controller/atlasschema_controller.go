4a8b57ccf22fe27118c56d4797fd8b8e1832fd18
Giau. Tran Minh <hello@giautm.dev>
2025-06-10 20:45:57 +0700

    internal/controller: fix status for AtlasSchema resource

--- internal/controller/atlasschema_controller.go
+++ internal/controller/atlasschema_controller.go
@@ -403,6 +403,7 @@ func (r *AtlasSchemaReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	if p := reports[0].Plan; p != nil {
 		s.PlanLink = p.File.Link
 		s.PlanURL = p.File.URL
+		reports[0].Plan = nil // Clear the plan to avoid big reports.
 	}
 	res.SetReady(s, reports[0])
 	r.recorder.Event(res, corev1.EventTypeNormal, "Applied", "Applied schema")
