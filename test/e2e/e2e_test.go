2d7e1fb21266d8555a83fc102b4043344fb7224f
noamcattan <ndcattan@gmail.com>
2025-07-22 12:07:28 +0300

    fix

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -78,7 +78,7 @@ func TestOperator(t *testing.T) {
 		)
 		require.NoError(t, err)
 		pods := strings.Split(output, " ")
-		if len(pods) == 1 {
+		if len(pods) == 1 && pods[0] != "" {
 			fmt.Fprintf(os.Stdout, "found controller-manager pod: %q\n", pods[0])
 			controllerPod = pods[0]
 			break
